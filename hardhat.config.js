d5013b9385a4108d4d0d920febcd3b781a126931
unknown <cloverdev88@gmail.com>
2022-08-23 05:36:38 -0400

    add doge chain network

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-etherscan");
 require("@openzeppelin/hardhat-upgrades");
 
-const { API_URL_MUMBAI, PRIVATE_KEY, POLYGON_API_KEY, API_URL_POLYGON, URL_MUMBAI } = process.env;
+const { API_URL_MUMBAI, PRIVATE_KEY, POLYGON_API_KEY, API_URL_POLYGON, URL_MUMBAI, URL_DOGE_CHAIN_TEST } = process.env;
 module.exports = {
   // solidity: {
   //   version: "0.7.6",
@@ -48,7 +48,13 @@ module.exports = {
       url: URL_MUMBAI,
       // url: API_URL_MUMBAI,
       accounts: [`0x${PRIVATE_KEY}`],
-      gasPrice: 250000000000,
+      // gasPrice: 250000000000,
+    },
+    doge_testnet: {
+      url: URL_DOGE_CHAIN_TEST,
+      // url: API_URL_MUMBAI,
+      accounts: [`0x${PRIVATE_KEY}`],
+      // gasPrice: 250000000000,
     },
   },
   etherscan: {
